a2652a72803aad119b4b954df2936e0b2acf2f60
Victor Bondarenko <nirasimha@gmail.com>
2022-04-20 09:36:24 -0400

    add event listener to 2 buttons

---
+++ app.js
@@ -0,0 +1,9 @@
+const p1Button = document.querySelector('#p1Button');
+const p2Button = document.querySelector('#p2Button');
+
+p1Button.addEventListener('click', () => {
+  alert('You clicked p1');
+});
+p2Button.addEventListener('click', () => {
+  alert('You clicked p2');
+});
