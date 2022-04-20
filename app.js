678da28a7396eebd0851cc74b8c780347c0dfcc0
Victor Bondarenko <nirasimha@gmail.com>
2022-04-20 10:02:27 -0400

    add prevent scores if the game is over

--- app.js
+++ app.js
@@ -5,16 +5,28 @@ const pingpong2 = document.querySelector('#pingpong2');
 
 let p1Score = 0;
 let p2Score = 0;
+let topScore = 5;
+let isGameOver = false;
 
 p1Button.addEventListener('click', () => {
   // console.log('You clicked p1');
-  p1Score++;
-  pingpong1.textContent = p1Score;
+  if(!isGameOver) {
+    p1Score++;
+      if(p1Score === topScore) {
+        isGameOver = true;
+      }
+    pingpong1.textContent = p1Score;
+  };
   // console.log(p1Score);
 });
 p2Button.addEventListener('click', () => {
   // console.log('You clicked p2');
-  p2Score++;
-  pingpong2.textContent = p2Score;
+  if(!isGameOver) {
+    p2Score++;
+      if(p2Score === topScore) {
+        isGameOver = true;
+      }
+    pingpong2.textContent = p2Score;
+  };
   // console.log(p2Score);
 });
