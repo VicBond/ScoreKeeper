7b9882a6cae4850896e055d083b88084f0798b87
Victor Bondarenko <nirasimha@gmail.com>
2022-04-20 09:39:12 -0400

    add score acc

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
 
+let p1Score = 0;
+let p2Score = 0;
+
 p1Button.addEventListener('click', () => {
-  alert('You clicked p1');
+  console.log('You clicked p1');
+  p1Score++;
+  console.log(p1Score);
 });
 p2Button.addEventListener('click', () => {
-  alert('You clicked p2');
+  console.log('You clicked p2');
+  p2Score++;
+  console.log(p2Score);
 });
