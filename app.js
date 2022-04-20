ab92f17ea1b86b2f6170a8e12208c3be73a08cb3
Victor Bondarenko <nirasimha@gmail.com>
2022-04-20 09:42:48 -0400

    change text content of  scores when clicked

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
+const pingpong1 = document.querySelector('#pingpong1');
+const pingpong2 = document.querySelector('#pingpong2');
 
 let p1Score = 0;
 let p2Score = 0;
 
 p1Button.addEventListener('click', () => {
-  console.log('You clicked p1');
+  // console.log('You clicked p1');
   p1Score++;
-  console.log(p1Score);
+  pingpong1.textContent = p1Score;
+  // console.log(p1Score);
 });
 p2Button.addEventListener('click', () => {
-  console.log('You clicked p2');
+  // console.log('You clicked p2');
   p2Score++;
-  console.log(p2Score);
+  pingpong2.textContent = p2Score;
+  // console.log(p2Score);
 });
